23ba1c6800ad2121994e3a33b22ec6e73d69038f
Ctomes <72369162+Ctomes@users.noreply.github.com>
2022-10-22 20:05:21 -0700

    added outline

---
+++ number-guessing-game.c
@@ -0,0 +1,15 @@
+//create a loop GAME_LOOP
+
+// prompt user for input
+
+// if 1, begin guessing game.
+    //loop
+    //prompt user for input
+    //notify player if exact; "win game" , lower or higher.
+    //on q. or win scenario go back to GAME_LOOP.
+//if 2 modify MAX.
+    // prompt player for new max.
+    //begin game part 1 with new Max.
+
+//if 3 quit program.
+
